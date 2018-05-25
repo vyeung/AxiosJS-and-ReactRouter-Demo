e23ec56fe5096b08be568002609d7fa11a68bed3
vyeung <vl.yeung@yahoo.com>
2018-05-25 16:00:24 -0400

    added Axios interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Axios from "axios";
+
+//common use case here is to add headers
+Axios.interceptors.request.use(requestConfig => {
+  console.log(requestConfig);
+  //whole point is to edit the request config before returning it
+  return requestConfig;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+Axios.interceptors.response.use(responseConfig => {
+  console.log(responseConfig);
+  //can also edit response config before returning it
+  return responseConfig;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
