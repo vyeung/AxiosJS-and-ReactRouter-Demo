52560456bd92efc92bcbcd90ccd97e55377079be
vyeung <vl.yeung@yahoo.com>
2018-05-25 16:41:15 -0400

    added option to create/use axios instances

---
+++ src/axiosInstance.js
@@ -0,0 +1,12 @@
+import Axios from "axios";
+
+const instance = axios.create({
+  baseURL: "some other website or route"
+});
+
+instance.defaults.headers.common["Auth"] = "Auth From Instance";
+
+//can also add interceptors in the instance
+//instance.interceptors.request...
+
+export default instance;
