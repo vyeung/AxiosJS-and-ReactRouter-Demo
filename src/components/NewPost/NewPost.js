6ecdef8dd10e3f38495b89732d27fdf6db0fcb4f
vyeung <vl.yeung@yahoo.com>
2018-05-25 13:41:48 -0400

    added ability to send POST request to server

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './NewPost.css';
+import Axios from "axios";
 
 class NewPost extends Component {
   state = {
     title: '',
     content: '',
     author: 'Max'
+  }
+
+  //sending a POST request
+  addPostHandler = () => {
+    const newPost = {
+      title: this.state.title,
+      body: this.state.content,
+      author: this.state.author
+    };
+    
+    Axios.post("https://jsonplaceholder.typicode.com/posts", newPost)
+      .then(response => {
+        console.log(response);
+      });
   }
 
   render() {
@@ -29,7 +44,7 @@ class NewPost extends Component {
           <option value="Max">Max</option>
           <option value="Manu">Manu</option>
         </select>
-        <button>Add Post</button>
+        <button onClick={this.addPostHandler}>Add Post</button>
       </div>
     );
   }
