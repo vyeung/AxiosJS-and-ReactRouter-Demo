b979a2df4e7087e641fd8635b84eb9db8ca2111c
vyeung <vl.yeung@yahoo.com>
2018-05-25 14:08:14 -0400

    added ability to send DELETE request to server

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import './FullPost.css';
+import Axios from "axios";
 
-class FullPost extends Component {
+class FullPost extends Component 
+{
+  //sending a DELETE request to server
+  deletePostHandler = () => {
+    Axios.delete("https://jsonplaceholder.typicode.com/posts/" + this.props.selectedPost[0].id)
+      .then(response => {
+        console.log(response);
+      });
+  }
+  
   render() {
     let post = <p className="paraCenter">Please select a Post from above!</p>;
     
@@ -12,7 +22,7 @@ class FullPost extends Component {
           <h1>{this.props.selectedPost[0].title}</h1>
           <p>{this.props.selectedPost[0].body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
           </div>
         </div>
       );
