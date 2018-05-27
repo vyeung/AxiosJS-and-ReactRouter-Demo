cb22bacd93097d8f02c717a8843c0f8eccfa6138
vyeung <vl.yeung@yahoo.com>
2018-05-27 18:45:34 -0400

    outsourced most of Blog.js to Posts.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,64 +1,9 @@
 import React, { Component } from 'react';
 import './Blog.css';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import Axios from "axios";
+import Posts from "./Posts/Posts";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPost: null,
-    hasError: false
-  }
-  
-  componentDidMount() 
-  {
-    //sending a GET request to server
-    Axios.get("/posts")
-      //will have all the data at this point  
-      .then(response => {
-        const getFirstFour = response.data.slice(0, 4);
-        const addAuthor = getFirstFour.map(postElement => {
-          //new js obj with author field added / "concatenated"
-          return {
-            ...postElement,
-            author: "Joe"
-          }
-        });
-        this.setState({posts: addAuthor});  //trigger re-render
-        console.log(response);
-      })
-      .catch(error => {
-        this.setState({hasError: true});
-      });
-  }
-
-  postSelectedHandler = (id) => {
-    const postToPass = this.state.posts.filter(post => post.id === id);
-    this.setState({selectedPost: postToPass});
-    console.log(postToPass);
-  }
-  
   render() {
-    //render setup of data we fetched
-    let postsArray;
-    
-    if(this.state.hasError === true) {
-      postsArray = <p className="blogParaCenter">Couldn't Get Posts!</p>
-    }
-    else {
-      postsArray = this.state.posts.map(postElement => {
-        return ( 
-          <Post 
-            key={postElement.id} 
-            myTitle={postElement.title} 
-            myAuthor={postElement.author}
-            clicked={() => this.postSelectedHandler(postElement.id)} />
-        );
-      });
-    }
-
     return (
       <div className="Blog">
         <header>
@@ -74,18 +19,7 @@ class Blog extends Component {
           </nav>
         </header>
 
-        <section className="Posts">
-          {postsArray}
-        </section>
-
-        <section>
-          {/*selectedPost is of type [{...}]*/}
-          <FullPost selectedPost={this.state.selectedPost}/>
-        </section>
-        
-        <section>
-          <NewPost />
-        </section>
+        <Posts />
       </div>
     );
   }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,69 @@
+import React, {Component} from "react";
+import "./Posts.css";
+import SinglePost from "../../../components/SinglePost/SinglePost";
+import axios from "axios";
+
+class Posts extends Component {
+  state = {
+    posts: [],
+    //selectedPost: null,
+    hasError: false
+  }
+
+  componentDidMount() 
+  {
+    //sending GET request to server
+    axios.get("/posts")
+      //will have all the data at this point  
+      .then(response => {
+        const getFirstFour = response.data.slice(0, 4);
+        const addedAuthor = getFirstFour.map(postElement => {
+          //new js obj with author field added / "concatenated"
+          return {
+            ...postElement,
+            author: "Joe"
+          }
+        });
+        this.setState({posts: addedAuthor});  //trigger re-render
+        console.log(response);
+      })
+      .catch(error => {
+        this.setState({hasError: true});
+        console.log(error);
+      });
+  }
+
+  postSelectedHandler = (id) => {
+    const postToPass = this.state.posts.filter(post => post.id === id);
+    this.setState({selectedPost: postToPass});
+    console.log(postToPass);
+  }
+  
+  render() {
+    let fetchedPosts;
+    if(this.state.hasError === true) {
+      fetchedPosts = <p className="postsParaCenter">Couldn't Get Posts!</p>
+    }
+    else {
+      //will be of type []
+      fetchedPosts = this.state.posts.map(postElement => {
+        return ( 
+          <SinglePost 
+            key={postElement.id} 
+            myTitle={postElement.title} 
+            myAuthor={postElement.author}
+            clicked={() => this.postSelectedHandler(postElement.id)} 
+          />
+        );
+      });
+    }
+
+    return (
+      <section className="Posts">
+        {fetchedPosts}
+      </section>
+    );
+  }
+}
+
+export default Posts;
